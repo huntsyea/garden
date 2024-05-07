27875a8f783b21602593a036792bf75f7fd489aa
Hunter Yeagley <hunter.c.yeagley@outlook.com>
2024-05-07 15:49:08 -0500

    Fixing styles

--- quartz/components/PageTitle.tsx
+++ quartz/components/PageTitle.tsx
@@ -7,9 +7,9 @@ const PageTitle: QuartzComponent = ({ fileData, cfg, displayClass }: QuartzCompo
   const title = cfg?.pageTitle ?? i18n(cfg.locale).propertyDefaults.title
   const baseDir = pathToRoot(fileData.slug!)
   return (
-    <h1 class={classNames(displayClass, "page-title")}>
+    <h2 class={classNames(displayClass, "page-title")}>
       <a href={baseDir}>{title}</a>
-    </h1>
+    </h2>
   )
 }
 
--- quartz/i18n/locales/en-US.ts
+++ quartz/i18n/locales/en-US.ts
@@ -30,7 +30,7 @@ export default {
       darkMode: "Dark mode",
     },
     explorer: {
-      title: "Explorer",
+      title: "Notes",
     },
     footer: {
       createdWith: "Created by",
